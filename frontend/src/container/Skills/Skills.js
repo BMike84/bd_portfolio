3aad2f707d7df925e47aa91d39d585cf5d188d1f
Michael Buffone <m_buffone@hotmail.com>
2022-12-19 08:36:58 -0500

    started skills

--- frontend/src/container/Skills/Skills.js
+++ frontend/src/container/Skills/Skills.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import ReactTooltip from "react-tooltip";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Skills.scss";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+      <div className="app__skills-container">
+        <motion.div
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__skills-item app__flex"
+        ></motion.div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
