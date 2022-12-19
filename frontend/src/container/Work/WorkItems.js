d480c475c7f8155f989b1b659bdd953074934fe8
Michael Buffone <m_buffone@hotmail.com>
2022-12-19 08:30:25 -0500

    added work section missing adding all relavent links description and image for project

---
+++ frontend/src/container/Work/WorkItems.js
@@ -0,0 +1,61 @@
+import { images } from "../../constants";
+const WorkItems = [
+  {
+    title: "Routes Clothes",
+    url: "",
+    code: "",
+    image: images.about01,
+    description: "Website",
+    tag: ["All", "shopify"],
+  },
+  {
+    title: "Galaxy Box",
+    url: "",
+    code: "",
+    description: "Website",
+    image: images.about01,
+    tag: ["All", "shopify"],
+  },
+  // {
+  //   title: "Cafe Bernie",
+  //   url: "",
+  // code: '',
+  // description: 'Website',
+  //   image: "",
+  //   tag: ["All", "shopify"],
+  // },
+  {
+    title: "Gericht",
+    url: "https://buffone-development-restaurant.netlify.app/",
+    code: "",
+    description: "Website",
+    image: images.about01,
+    tag: ["All", "ui/ux"],
+  },
+  {
+    title: "GPT-3",
+    url: "https://buffone-development-gtp3.netlify.app/",
+    code: "",
+    description: "Website",
+    image: images.about01,
+    tag: ["All", "ui/ux"],
+  },
+  {
+    title: "Cut The Line",
+    url: "https://github.com/BMike84/cut-the-line",
+    code: "",
+    description: "Website",
+    image: images.about01,
+    tag: ["All", "web app"],
+  },
+  {
+    title: "The Vault",
+    url: "https://github.com/BMike84/TheVault",
+    code: "",
+    description: "Website",
+    image: images.about01,
+    tag: ["All", "web app"],
+  },
+];
+
+export default WorkItems;
