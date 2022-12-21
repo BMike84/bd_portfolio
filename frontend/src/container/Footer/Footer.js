668cc9adb7ebf659c821769c7366c9c1d8f0dbff
Michael Buffone <m_buffone@hotmail.com>
2022-12-21 15:51:55 -0500

    added footer section using email.js

--- frontend/src/container/Footer/Footer.js
+++ frontend/src/container/Footer/Footer.js
@@ -1,8 +1,127 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { AiFillPhone } from "react-icons/ai";
+import emailjs from "@emailjs/browser";
+import { AppWrap, MotionWrap } from "../../wrapper";
+
+import "./Footer.scss";
+
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
-};
+  const form = useRef();
 
-export default Footer;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    setLoading(true);
+
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3gaw26l",
+        "template_31bbt7t",
+        form.current,
+        "PUrMfxdGI8-Tr0y_c"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setLoading(false);
+          setIsFormSubmitted(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <h2 className="head-text">
+        Have Any <span>Questions?</span>
+      </h2>
+      <h2 className="head-text">
+        <span>Contact</span> Me<span>!</span>
+      </h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <p className="email-icon">
+            <HiOutlineMail />
+          </p>
+          <a href="mailto:michael@michaelbuffone.net" className="p-text">
+            michael@michaelbuffone.net
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <p>
+            <AiFillPhone />
+          </p>
+          <a href="tel:+1 (514) 574-6006" className="p-text">
+            +1 (514) 574-6006
+          </a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <form className="app__footer-form app__flex" ref={form}>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={name}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? "Sending" : "Send Message"}
+          </button>
+        </form>
+      ) : (
+        <div>
+          <h3 className="head-text">Thank you for getting in touch</h3>
+        </div>
+      )}
+    </>
+  );
+};
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__secondarybg"
+);
